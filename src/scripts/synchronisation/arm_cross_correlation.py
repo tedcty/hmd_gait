9a6239d9eba0b04b371b5e2d65cece9319378f16
vibhava-l <vibhava.leelaratna@gmail.com>
2025-06-07 20:25:01 +1200

    Changes to participant ID and trial

--- src/scripts/synchronisation/arm_cross_correlation.py
+++ src/scripts/synchronisation/arm_cross_correlation.py
@@ -41,8 +41,8 @@ def read_kinematic_data(kinematic, participant_id, session_id):
 
 
 if __name__ == '__main__':
-    participant_id = 'P001' # NOTE: Replace with the actual participant ID
-    session_id = 'Reactive normal 1' # NOTE: Replace with the actual session ID
+    participant_id = 'P019' # NOTE: Replace with the actual participant ID
+    session_id = 'Straight VR 3' # NOTE: Replace with the actual session ID
 
     # Read both IMUs and kinematic data
     # NOTE: Change to left leg if needed
