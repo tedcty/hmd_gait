9850a10628598ca90656feb3e19de2e497b25c48
tedcty <tedyeung@outlook.com>
2025-06-12 08:01:59 +1200

    copy function

--- src/scripts/nature_paper.py
+++ src/scripts/nature_paper.py
@@ -14,7 +14,7 @@ def worker(args):
     except IOError:
         print("IO Error copying {0} to {1}".format(sour, targ))
 
-if __name__ == '__main__':
+def copy_imu_files():
     work_dir = "M:/Mocap/Movella_Re/"
     out_dir = "I:/Meta/"
     participants = [f for f in os.listdir(work_dir)]
@@ -26,6 +26,50 @@ if __name__ == '__main__':
         for t in trials:
             print("{0}: {1}".format(p, t))
             imus = [f for f in os.listdir("{0}{1}/{2}".format(work_dir, p, t)) if f.endswith(".csv.pkl")]
+            imus_size = [os.path.getsize("{0}{1}/{2}/{3}".format(work_dir, p, t, f)) for f in imus]
+            trials_size.append(np.sum(imus_size))
+            pass
+        participants_size.append(np.sum(trials_size))
+    part_size = np.sum(participants_size)
+    part_kb = part_size / 1024.0
+    part_MB = part_kb / 1024.0
+    part_GB = part_MB / 1024.0
+
+    print("Data to be copied {0:.2f} GB".format(np.round(part_GB, 2)))
+
+    copier = []
+
+    print("Creating jobs")
+    for p in participants:
+        trials = [f for f in os.listdir("{0}{1}".format(work_dir, p))]
+        for t in trials:
+            imus = [f for f in os.listdir("{0}{1}/{2}".format(work_dir, p, t)) if f.endswith(".csv.pkl")]
+            for i in imus:
+                so = "{0}{1}/{2}/{3}".format(work_dir, p, t, i)
+                ta = "{0}{1}/{2}/{3}".format(out_dir, p, t, i)
+
+                if not os.path.exists("{0}{1}/{2}/".format(out_dir, p, t)):
+                    os.makedirs("{0}{1}/{2}/".format(out_dir, p, t))
+                copier.append([so, ta])
+            pass
+
+    print("Copying ...")
+    poo = Pool(12)
+    poo.map(worker, copier)
+    print("Done!")
+
+if __name__ == '__main__':
+    work_dir = "M:/Mocap/"
+    out_dir = "I:/Meta/Mocap/"
+    participants = [f for f in os.listdir(work_dir) if f.lower().startswith('p')]
+    participants_size = []
+    print("Scanning for data in: {0}".format(work_dir))
+    for p in participants:
+        trials = [f for f in os.listdir("{0}{1}".format(work_dir, p)) if os.path.isdir("{0}{1}/{2}".format(work_dir, p, f))]
+        trials_size = []
+        for t in trials:
+            print("{0}: {1}".format(p, t))
+            imus = [f for f in os.listdir("{0}{1}/{2}".format(work_dir, p, t)) if f.endswith(".trc")]
             imus_size = [os.path.getsize("{0}{1}/{2}/{3}".format(work_dir, p, t, f)) for f in imus]
             trials_size.append(np.sum(imus_size))
             pass
@@ -43,15 +87,15 @@ if __name__ == '__main__':
 
     print("Creating jobs")
     for p in participants:
-        trials = [f for f in os.listdir("{0}{1}".format(work_dir, p))]
+        trials = [f for f in os.listdir("{0}{1}".format(work_dir, p)) if os.path.isdir("{0}{1}/{2}".format(work_dir, p, f))]
         for t in trials:
-            imus = [f for f in os.listdir("{0}{1}/{2}".format(work_dir, p, t)) if f.endswith(".csv.pkl")]
+            imus = [f for f in os.listdir("{0}{1}/{2}".format(work_dir, p, t)) if f.endswith(".trc")]
             for i in imus:
                 so = "{0}{1}/{2}/{3}".format(work_dir, p, t, i)
-                ta = "{0}{1}/{2}/{3}".format(out_dir, p, t, i)
+                ta = "{0}{1}/{3}".format(out_dir, p, t, i)
 
-                if not os.path.exists("{0}{1}/{2}/".format(out_dir, p, t)):
-                    os.makedirs("{0}{1}/{2}/".format(out_dir, p, t))
+                if not os.path.exists("{0}{1}/".format(out_dir, p)):
+                    os.makedirs("{0}{1}/".format(out_dir, p))
                 copier.append([so, ta])
             pass
 
