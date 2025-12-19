31bb79c3eaff52f4cacfa2d2a1cd3935d2a8521e
Vibhava Leelaratna <vlee669@UoA.auckland.ac.nz>
2025-12-19 16:35:16 +1300

    Changed filepaths from HPC to network drive

--- src/scripts/pca_fit_newdata.py
+++ src/scripts/pca_fit_newdata.py
@@ -789,10 +789,10 @@ class DeviationAnalysis:
 
 if __name__ == "__main__":
     # Configuration
-    out_root = "/hpc/vlee669/Results/30 Participants/features"
-    results_dir = "/hpc/vlee669/Results/30 Participants/models"
-    minimal_results_dir = "/hpc/vlee669/Results/30 Participants/minimal_imu_models"
-    output_dir = "/hpc/vlee669/Results/30 Participants/deviation_analysis"
+    out_root = "Z:/Upper Body/Results/30 Participants/features"
+    results_dir = "Z:/Upper Body/vlee669/Results/30 Participants/models"
+    minimal_results_dir = "Z:/Upper Body/vlee669/Results/30 Participants/minimal_imu_models"
+    output_dir = "Z:/Upper Body/vlee669/Results/30 Participants/deviation_analysis"
 
     datatype = "IMU"
     minimal_imu_set = True  # Use only Head_imu and RightForeArm_imu
